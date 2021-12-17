2bfb26f58f3a57e2c077249351ec9770000ec0c9
vecpeng <841298391@qq.com>
2021-12-17 17:23:09 +0000

    fix return value to user app

--- kernel/strap.c
+++ kernel/strap.c
@@ -22,7 +22,8 @@ static void handle_syscall(trapframe *tf) {
   // kernel/syscall.c) to conduct real operations of the kernel side for a syscall.
   // IMPORTANT: return value should be returned to user app, or else, you will encounter
   // problems in later experiments!
-  do_syscall(tf->regs.a0, tf->regs.a1, 0, 0, 0, 0, 0, 0);
+  long code = do_syscall(tf->regs.a0, tf->regs.a1, 0, 0, 0, 0, 0, 0);
+  tf->regs.a0 = code;
 }
 
 //
