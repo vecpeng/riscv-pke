1d2e9b794f51033e5f5f19d814c3ee751ad7cf14
vecpeng <841298391@qq.com>
2021-12-17 11:14:22 +0000

    fix lab1_2

--- kernel/strap.c
+++ kernel/strap.c
@@ -22,8 +22,7 @@ static void handle_syscall(trapframe *tf) {
   // kernel/syscall.c) to conduct real operations of the kernel side for a syscall.
   // IMPORTANT: return value should be returned to user app, or else, you will encounter
   // problems in later experiments!
-  panic( "call do_syscall to accomplish the syscall and lab1_1 here.\n" );
-
+  do_syscall(tf->regs.a0, tf->regs.a1, 0, 0, 0, 0, 0, 0);
 }
 
 //
