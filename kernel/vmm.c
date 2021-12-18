bc43507355d5b9f030444ef225ce795e0a6c9f43
vecpeng <841298391@qq.com>
2021-12-18 03:39:42 +0000

    my work on lab2_2 is done.

--- kernel/vmm.c
+++ kernel/vmm.c
@@ -184,6 +184,6 @@ void user_vm_unmap(pagetable_t page_dir, uint64 va, uint64 size, int free) {
   // (use free_page() defined in pmm.c) the physical pages. lastly, invalidate the PTEs.
   // as naive_free reclaims only one page at a time, you only need to consider one page
   // to make user/app_naive_malloc to produce the correct hehavior.
-  panic( "You have to implement user_vm_unmap to free pages using naive_free in lab2_2.\n" );
-
+  uint64 pa = lookup_pa(page_dir, va);
+  free_page((void *)pa);
 }
